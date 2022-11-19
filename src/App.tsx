faf1783a43bfd4084a137120f304338e940ca0bc
Seweryn Buksa <seweryn@volt.io>
2022-11-19 17:45:31 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import NavigationMenu from "./components/NavigationMenu/NavigationMenu";
 import Page from "./components/Page/Page";
 
 export default function App() {
+  const CLIENT_ID = import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID;
+
   return (
-    <div className="App">
-      <Router>
-        <NavigationMenu />
-        <Page />
-      </Router>
-    </div>
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <div className="App">
+        <Router>
+          <NavigationMenu />
+          <Page />
+        </Router>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
